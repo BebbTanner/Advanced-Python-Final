e34de565cd5c75d0b079826bb4a780aef25a4cc4
Tanner Bebb <17tbebb@gmail.com>
2023-12-06 11:03:17 -0500

    Added documentation for lines 21-23, lines 27-31, lines 34-37

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 '''
-The purpose of this program is to create results of a tournamet and post them to a website.
+The purpose of this program is to create results of a tournament and post them to a website.
 This is done by using the start.gg API and a python wrapper called pysmashgg.
 '''
 
@@ -8,24 +8,33 @@ from config import smash_key
 import pprint
 import datetime
 import json
-from LGLSData import LGLS_1_Players
 
 '''
 This is an empty list that will be used to store the tournament info
-that I will be grabing from startgg's site.
+that I will be grabbing from startgg's site.
 '''
 haysTournaments = []
 
 smash = pysmashgg.SmashGG(smash_key, True)
 for i in range(1, 10):
     '''
-    
+    The command on line 24 is sorting all of the tournaments by state.
+    It is only finding the tournaments in the state of Kansas.
     '''
     tournaments_by_state = smash.tournament_show_by_state('KS',  i)
 
     if tournaments_by_state != []:
+        '''
+        This is further sorting through the tournaments.
+        Once it has found all of the tournaments in the state of Kansas,
+        it is then looking for all of the tournaments in the city of Hays.
+        '''
         for tourney in tournaments_by_state:
             if tourney['city'] == 'Hays':
+                '''
+                After the program has found those tournaments in Hays, It is adding 
+                them to the list that I created at the beginning of the program.
+                '''
                 haysTournaments.append(tourney)
 
 for tourney in haysTournaments:
