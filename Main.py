52b8f2bf98bffa2553ce2b2970423ee8fa5d4fd5
Tanner Bebb <17tbebb@gmail.com>
2023-12-06 10:48:45 -0500

    Added documentation for lines 1-4 and lines 13-16

--- Main.py
+++ Main.py
@@ -1,3 +1,8 @@
+'''
+The purpose of this program is to create results of a tournamet and post them to a website.
+This is done by using the start.gg API and a python wrapper called pysmashgg.
+'''
+
 import pysmashgg
 from config import smash_key
 import pprint
@@ -5,27 +10,27 @@ import datetime
 import json
 from LGLSData import LGLS_1_Players
 
+'''
+This is an empty list that will be used to store the tournament info
+that I will be grabing from startgg's site.
+'''
 haysTournaments = []
-#this will import the API key from the config file
-#This will allow you to make the call to the website that will grab the LGLS info
+
 smash = pysmashgg.SmashGG(smash_key, True)
 for i in range(1, 10):
-    #this command will print out active tournements in the state you choose
+    '''
+    
+    '''
     tournaments_by_state = smash.tournament_show_by_state('KS',  i)
-    #pprint.pprint(tournaments_by_state)
 
     if tournaments_by_state != []:
         for tourney in tournaments_by_state:
             if tourney['city'] == 'Hays':
                 haysTournaments.append(tourney)
 
-#pprint.pprint(haysTournaments)
-#print("\n\n\n")
-
 for tourney in haysTournaments:
     events = smash.tournament_show_events(tourney['slug'])
     tourney['events'] = events
-    #pprint.pprint(events)
 
     for event in tourney['events']:
         results = smash.tournament_show_lightweight_results(tourney['slug'],event['slug'], 1)
